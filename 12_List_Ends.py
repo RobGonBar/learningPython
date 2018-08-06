5a235e9119bd7407143eb75d19a027e5f8a34e5b
RobGonBar <rogbar@gmail.com>
2018-08-05 21:02:39 -0400

    Created list  ends program

---
+++ 12_List_Ends.py
@@ -0,0 +1,12 @@
+#Function to output the first and last elements in a list
+
+import random
+
+a = []
+
+def firstLast():
+    while(len(a) < random.randint(1,15):
+        a.append(random.randint(1,30)
+
+firstLast()
+
