73802dfffcd45b6474ba2aae458524183bc85f14
RobGonBar <rogbar@gmail.com>
2018-08-07 20:45:59 -0400

    Created Fibonacci program

---
+++ 13_Fibonacci.py
@@ -0,0 +1,24 @@
+#Asks the user how many Fibonacci numbers to create and then generates them
+
+
+def askUser():
+    userNumber = int(input("How many Fibonacci numbers do you want to generate? "))
+    return userNumber
+
+def createSequence(number):
+    sequence = []
+    x = range(2,number)
+    i = 1
+
+    if number == 1:
+        sequence = [1]
+    elif number == 2:
+        sequence = [1,1]
+    else:
+        sequence = [1,1]
+        while len(sequence) < number:
+            sequence.append(sequence[i] + sequence[i-1])
+            i += 1
+    print(sequence)
+
+createSequence(askUser())
