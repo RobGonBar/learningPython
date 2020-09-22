6d3c8bb06d52287f1e2a1625ef7dbc7d9b5a84cc
RobGonBar <robgbar@gmail.com>
2020-09-22 10:01:05 -0400

    messed around with classes and variables

--- Random.py
+++ Random.py
@@ -1,9 +1,33 @@
 class vulnerability:
+
+    num_of_vulns = 0
+
     def __init__(self,vulnID,category):
         self.vulnID = vulnID
         self.category = category
 
-FIPS = vulnerability('v23243','II')
+        self.status = "open"
 
-print(FIPS.category)
-print(FIPS.vulnID)
+        vulnerability.num_of_vulns += 1
+
+    def changeStatus(self):
+        if(self.status == "open"):
+            self.status = "closed"
+        else:
+            self.status = "open"
+
+
+
+
+v1 = vulnerability('v23243','II')
+v2 = vulnerability('v23566','I')
+v3 = vulnerability('v27866','I')
+
+print(v1.status)
+print(v2.__dict__)
+print(v3.__dict__)
+print(v3.num_of_vulns)
+vulnerability.changeStatus(v3)
+print(v3.__dict__)
+vulnerability.changeStatus(v3)
+print(v3.__dict__)
